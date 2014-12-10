2b24fd899579f13cd19a7fe2bad98d1cb26ee711
Nick Herman <gerkola@gmail.com>
2014-12-11 02:25:51 +0300

    Add threading and nesting for lab#2.
    TODO:
     * add proper output (rewrite output concurrency)
     * add tests
     * add relative urls support
     * change input format (1 url => file of urls)

--- lab2/project.clj
+++ lab2/project.clj
@@ -1,7 +1,8 @@
 (defproject lab2 "0.1.0-SNAPSHOT"
   :description "Multithreaded recursive crowler bot."
   :dependencies [[org.clojure/clojure "1.6.0"],
-                 [http-kit "2.1.16"]]
+                 [http-kit "2.1.16"]
+                 [com.climate/claypoole "0.3.3"]]
   :main ^:skip-aot lab2.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- lab2/src/lab2/core.clj
+++ lab2/src/lab2/core.clj
@@ -1,6 +1,9 @@
 (ns lab2.core
-  (:require [org.httpkit.client :as http])
+  (:require [org.httpkit.client :as http]
+            [com.climate.claypoole :as cp])
   (:gen-class))
+
+(def pool (cp/threadpool 20))
 
 (defn url-without-ref [url]
   (first (clojure.string/split url #"#")))
@@ -16,35 +19,63 @@
 
 (defn node-for [url, {:keys [status headers body error] :as resp}]
   (if error
-    [[url 0 "bad" []] []]
+    [[url 0 "bad" nil] []]
     (if (= status 200)
-      (let [links (find-links body url)
-            cnt (count links)]
-        [[url cnt (str cnt) []] links])
+      (if (re-matches #".*html.*" (:content-type headers))
+        (let [links (find-links body url)
+              cnt (count links)]
+          [[url cnt (str cnt) (agent [])] links])
+      ;else
+        [[url 0 0 nil] []])
     ;else
-      [[url 0 "redir" []] []])))
+      [[url 0 "redir" nil] []])))
 
-(defn handle-req [[par-url par-sz par-msg par-children] url depth]
-  (let [resp @(http/get url)
-        [node urls] (node-for url resp)]
-    ;; (if (> 0 depth)
-    [par-url par-sz par-msg (cons (first (node-for url resp)) par-children)]))
-
+(defn handle-req [[par-url _ _ par-children :as par-node] url depth]
+  (if par-children
+    (let [resp @(http/get url {:follow-redirects false})
+          [node urls] (node-for url resp)
+                                        ; [par-url par-sz par-msg (cons (first (node-for url resp)) par-children)]
+          [child-node links] (node-for url resp)]
+      (do
+        (send par-children (partial cons child-node))
+        (if (< 0 depth)
+          (cp/pfor pool [url urls]
+                         (handle-req child-node url (dec depth))))))))
 
 (defn populate-node [par-node urls depth]
-  (reduce (fn [node url] (handle-req node url depth)) par-node urls))
+  (reduce (fn [node url]
+            (handle-req node url depth))
+          par-node
+          urls))
+
+(defn print-node [[par-url par-cnt par-str par-children] prefix]
+  (let [x (promise)]
+    (println prefix par-url par-str)
+    (if par-children
+      (add-watch par-children
+                 :key
+                 (fn [key _ _ children]
+                   (if (= par-cnt (count children))
+                     (do
+                       (doseq [n children]
+                         (print-node n (clojure.string/join [prefix "  "])))
+                       (deliver x nil))))))
+    @x))
 
 (defn crawl [depth url]
   (let [resp @(http/get url)
         [node urls] (node-for url resp)]
-    (let [tree (populate-node node urls (dec depth))]
-      (do
-        (println tree)
-        (doseq [n (nth tree 3)]
-          (println (first n) (nth n 2)))))))
+    ;(let [tree (populate-node node urls (dec depth))]
+    (do
+      (cp/pfor pool [url urls]
+                     (handle-req node url (dec depth)))
+      (print-node node ""))))
 
 (defn -main [& args]
-  (case (count args)
-    2 (apply crawl (cons (int (first args)) (rest args)))
-    1 (apply crawl (cons 3 args))
-    (println "Format: lein run [depth] url")))
+  (do
+    (case (count args)
+      2 (apply crawl (cons (Integer/parseInt (first args)) (rest args)))
+      1 (apply crawl (cons 3 args))
+      (println "Format: lein run [depth] url"))
+    (cp/shutdown pool)
+    (shutdown-agents)))
